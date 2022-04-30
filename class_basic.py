f1785027c00ef8d76512f3adf2872de6ecd9bbdc
yosen-king <58020535+yosen-king@users.noreply.github.com>
2022-04-30 15:26:46 +0800

    Add files via upload

---
+++ class_basic.py
@@ -0,0 +1,307 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 16:16:41 2022
+
+@author: He Zekai
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+from streamlit_echarts import st_echarts
+
+
+from pyecharts.faker import Faker
+from streamlit_echarts import st_echarts, JsCode
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit.components.v1 as components
+
+import streamlit.components.v1 as components
+from pyecharts import options as opts
+from pyecharts.charts import Map
+from pyecharts.faker import Faker
+
+from LAC import LAC
+
+from pyecharts import options as opts
+from pyecharts.charts import *
+from pyecharts.charts import Bar
+from pyecharts.faker import Faker
+import streamlit_echarts
+from pyecharts import options as opts
+from pyecharts.globals import ThemeType
+#就业情况--矩阵树状图
+from pyecharts import options as opts
+from pyecharts.charts import TreeMap
+
+
+
+
+class Basic_imf():
+    def __init__(self,dataframe):
+        self.dataframe = dataframe
+    
+    def main(self):
+        df = self.dataframe
+        # 展示一级标题
+        st.header('学生用户画像')
+        
+        st.subheader('班级基本信息情况')
+        
+        
+        # df = pd.read_csv(r'data.csv') 
+        # df = df.drop([0])
+        
+        data = [
+            {"value":int(df['就业状态'].value_counts(sort=0)[1]), "name": "工作"},
+            {"value":int(df['就业状态'].value_counts(sort=0)[2]), "name": "出国"},
+            {"value":int(df['就业状态'].value_counts(sort=0)[2]), "name": "考研"},
+            {"value":int(df['就业状态'].value_counts(sort=0)[2]), "name": "公务员"},
+            {"value":int(df['就业状态'].value_counts(sort=0)[2]), "name": "未就业"}
+        ]
+        c = (
+            TreeMap()
+            .add("就业情况数据",data)
+            .set_global_opts(title_opts=opts.TitleOpts(title="就业状况"))
+        )
+        
+        streamlit_echarts.st_pyecharts(
+              c
+        )
+        
+        #生源地地图
+        # 装载分词模型
+        lac = LAC(mode='seg')
+        shengfen = []
+        for i in df['生源地']:
+            a =  lac.run(i)
+            shengfen.append(a[0])
+        sf = []
+        for i in  list(set(shengfen)):
+            sf.append([i,shengfen.count(i)])
+            
+        c = (
+            Map()
+            .add("生源数", sf, "china")
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="生源数地图"),
+                visualmap_opts=opts.VisualMapOpts(max_=50, is_piecewise=True),
+            )    .render_embed()
+        )
+        components.html(c, width=900, height=600)
+        
+        #四六级二级--堆积条形图
+        x_index = ["英语四级", "英语六级", "计算机二级"]
+        y_value1 = [int(df['四级通过'].value_counts()[0]), int(df['六级通过'].value_counts()[0]), int(df['计算机二级通过'].value_counts()[0])]
+        y_value1plus = [int(df['四级通过'].value_counts()[1]), int(df['六级通过'].value_counts()[1]), int(df['计算机二级通过'].value_counts()[1])]
+        
+        bar = (
+            Bar()
+            .add_xaxis(x_index)
+            .add_yaxis("通过", y_value1, stack = "stack1") # y轴设置
+            .add_yaxis("未通过", y_value1plus, stack = "stack1") # y轴设置
+            .set_global_opts(title_opts=opts.TitleOpts(title="技能考试通过情况"))
+            .set_series_opts(label_opts=opts.LabelOpts(is_show=False)) 
+            )
+        
+        bar.render_notebook()
+        streamlit_echarts.st_pyecharts(bar)
+        
+        
+        
+        
+        
+        
+        # def load_data():
+        #     data = pd.read_csv("data.csv")
+        #     return data
+        
+        
+        
+        # df = load_data()
+        
+        def getlistnum(li):#这个函数就是要对列表的每个元素进行计数
+            li = list(li)
+            set1 = set(li)
+            dict1 = {}
+            for item in set1:
+                dict1.update({item:li.count(item)})
+            return dict1
+        
+        dfhy = df.loc[:,["工作行业",]]
+        dfhy["工作行业"] = dfhy["工作行业"].astype(str)
+        
+        hydic=getlistnum(dfhy['工作行业'])
+        
+        hydic['未知']=hydic.pop('nan')
+        
+        dfhydata=list(hydic.keys())
+        
+        
+        col = ['value', 'name']
+        
+        hydataall=[{col[0]:hydic.get(dfhydata[p]),col[1]:dfhydata[p]
+            } for p in range(len(dfhydata))]
+        
+        
+        
+        
+        # for i in range(51):
+        #     a=dict(zip(df['平均绩点'],df['姓名']))
+        
+        dfjd = df.loc[:,['平均绩点','姓名',]]
+        dfjd["平均绩点"] = dfjd["平均绩点"].astype(float)  # 将销量列数据类型转换为float
+        
+        dfxf = df.loc[:,["校园卡消费总金额",]]
+        dfxm = df.loc[:,["姓名",]]
+        dfxf["校园卡消费总金额"] = dfxf["校园卡消费总金额"].astype(float)
+        
+        dfxfl = [np.array(dfxf).tolist()[i][0] for i in range(len(df))]
+        dfxml = [np.array(dfxm).tolist()[i][0] for i in range(len(df))]
+        
+        dfjd.columns = ['value', 'name']
+        user_info = [{col:dfjd[col].tolist()[i] for col in dfjd.columns} for i in range(len(df))]
+        
+        
+        
+        
+        
+        options = {
+            "title": {
+                "text": "校园卡消费金额"
+            },
+            "xAxis": {
+                "type": "category",
+                "data": dfxml  # 此处也可传入数组
+            },
+            "tooltip": {
+                "formatter": JsCode("function (params) \
+                    { return `${params.seriesName}<br />${params.name}：${params.value}` ;}").js_code
+            },
+            "yAxis": {
+                "type": "value"
+            },
+            "series": [
+                {
+                    "name": "fakerSeries",
+                    "data": dfxfl,  # 此处也可传入数组
+                    "type": "line"
+                }
+            ],
+        }
+        
+        events = {
+            "click": "function(params) { alert(params.name) }"
+        }
+        
+        st_echarts(options=options, events=events)
+        
+        
+        option = {
+            'title' : {
+                'text': '工作行业分布',
+                "top": "top",
+                'x':'left'
+            },
+            'tooltip' : {
+                'trigger': 'item',
+                'formatter': "{a} <br/>{b} : {c} ({d}%)"
+            },
+            'legend': {
+                'orient': 'vertical',
+                'left': 'left',
+                'bottom':10,
+                'data': dfhydata
+                
+            },
+            'series' : [
+                {
+                    'name': '行业分布',
+                    'type': 'pie',
+                    'radius' : '75%',
+                    #'center': ['50%', '60%'],
+                        'data':hydataall
+                    #    [
+                    #     {'value':335, 'name':'直接访问'},
+                    #     {'value':310, 'name':'邮件营销'},
+                    #     {'value':234, 'name':'联盟广告'},
+                    #     {'value':135, 'name':'视频广告'},
+                    #     {'value':1548, 'name':'搜索引擎'}
+                    # ]
+                    ,
+                    'itemStyle': {
+                        'emphasis': {
+                            'shadowBlur': 10,
+                            'shadowOffsetX': 0,
+                            'shadowColor': 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        st_echarts(options=option, height="500px")
+        
+        
+        option = {
+        "title": {
+                "text": "平均绩点",
+                "top": "top",
+                "left": "left"
+            },
+        "legend": {
+            "left": 'center',
+            "bottom":10,
+            "type":'scroll'
+        },
+        "toolbox": {
+            "show": "true",
+            "feature": {
+                "mark": {"show": "true"},
+                "dataView": {"show": "true", "readOnly": "false"},
+                "restore": {"show": "true"},
+                
+            }
+        },
+        "series": [
+            {
+                "name": '平均绩点',
+                "type": 'pie',
+                "radius": ["10", "180"],
+                #"center": ['50%', '30%'],
+                "roseType": 'area',
+                "itemStyle": {
+                    "borderRadius": "8"
+                },
+                    "data": user_info
+            }
+        ],
+        "tooltip": {
+                        "show": "true"
+                    },
+        "label": {
+            "show":"true"
+        },
+        };
+        
+        st_echarts(options=option, height="600px")
+        
+        
+        
+        
+        
+        # visualization = st.sidebar.selectbox('基本情况信息',('班级基本情况信息',"学生个人基本情况"))
+        # state_select = st.sidebar.selectbox('选择学生', df['姓名'].unique())
+        # status_select = st.sidebar.radio("预警系统", ('无','学业预警', '贫困预警'))
+
+
+
+
+
+
+
+
+
+
+
---
+++ try.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+@author:  YHH---TEAM
+
+
+the ensemble and main progress
+"""
+
+import streamlit as st
+from pathlib import Path
+import base64
+import streamlit.components.v1 as components
+from Personal import personal_graph   #个人画像主页
+import pandas as pd
+from class_basic import Basic_imf
+from Poverty_Alert import Alert
+
+st.set_page_config(layout="wide")
+# design the Structure of sidebar
+
+# 数据图区
+def read_data():
+    data = pd.read_csv('data.csv')
+    data = data.drop([0])
+    return data
+
+df = read_data()
+
+# 数理学院logo图片解析
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+# 管理员权限页面构造与功能实现
+def manager():
+    choise_manager = st.sidebar.select_slider('请选择一种验证方式',['人脸识别','0','账号密码输入'],value = '0',
+                                              help='通过验证后方才进入系统内部进行数据更改，拖动滑块选择一种验证方式')
+    if choise_manager == '账号密码输入':
+        accout = st.sidebar.text_input(label='管理员账号',help='请输入你的管理员账号密码信息',placeholder='ID')
+        password = st.sidebar.text_input(label='管理员密码',placeholder='PASSWORD')
+        confirm = st.sidebar.button('登录')
+        
+        if confirm:
+            if accout == 'root' and password == 'root':
+                st.sidebar.markdown('<right>`登录成功，请继续操作`<right>',unsafe_allow_html=True)
+                return None
+            else:
+                st.sidebar.markdown('<right>`账号或密码错误，请重新输入`<right>',unsafe_allow_html=True)
+                return None
+    if choise_manager == '人脸识别':
+        st.sidebar.camera_input("请对准摄像头",help='人脸识别验证方式需要您允许网页端使用当前设备摄像头')
+        return None
+        
+def sidebar():
+    st.sidebar.markdown('''[ <p style="text-align: center;"> <img src='data:image/png;base64,{}' class='img-fluid' width=300 height=72>](http://shuli.xy.bitzh.edu.cn/slxy/)'''.format(img_to_bytes("LOGO.png")), unsafe_allow_html=True)
+    st.sidebar.markdown('**<center>学生画像系统<center>**',unsafe_allow_html=True)
+    choise = st.sidebar.radio('功能选择',['宏观总结','个人画像','管理员权限'],index = 0)
+    if choise == '个人画像':
+        ano_choise = st.sidebar.select_slider('请选择一种预警方式', ['学业预警','0','贫困预警'],value = '0')
+        
+        if ano_choise=='学业预警':
+            name = st.sidebar.selectbox('请选择一位学生', df['姓名'].tolist())
+            st.progress(100)
+            st.spinner()
+            with st.spinner(text='词云图头像制作正在进行中'):
+                 personal_graph(name, df).main()   
+        
+        if ano_choise == '贫困预警':
+            st.progress(100)
+            st.spinner()
+            with st.spinner(text='贫困生信息处理正在进行中'):
+                 Alert(df).main()  
+        
+        return None
+    
+    if choise == '管理员权限':
+        manager()
+            
+                
+        return None
+    
+    if choise == '宏观总结':
+        st.progress(100)
+        st.spinner()
+        with st.spinner(text='班级信息正在进行中'):
+             Basic_imf(df).main()  
+        
+    
+    
+    # with st.form(key='my_form'):
+    #     text_input = st.text_input(label='Enter some text')
+    #     submit_button = st.form_submit_button(label='Submit')
+        
+        # st.sidebar.button('Hit me')
+        # # st.sidebar.download_button('On the dl', data)
+        # st.sidebar.checkbox('Check me out')
+        # st.sidebar.radio('Radio', [1,2,3])
+        # st.sidebar.selectbox('Select', [1,2,3])
+        # st.sidebar.multiselect('Multiselect', [1,2,3])
+        # st.sidebar.slider('Slide me', min_value=0, max_value=10)
+        # st.sidebar.select_slider('Slide to select', options=[1,'2'])
+        # st.sidebar.text_input('Enter some text')
+        # st.sidebar.number_input('Enter a number')
+        # st.sidebar.text_area('Area for textual entry')
+        # st.sidebar.date_input('Date input')
+        # st.sidebar.time_input('Time entry')
+        # st.sidebar.file_uploader('File uploader')
+        # st.sidebar.camera_input("一二三,茄子!")
+        # st.sidebar.color_picker('Pick a color')
+
+
+
+def main():
+    sidebar()
+    return None
+
+
+if __name__ == '__main__':
+    main()
+
+
+
